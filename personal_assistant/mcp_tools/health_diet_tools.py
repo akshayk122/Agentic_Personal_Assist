bcda224a5a1aedb12eb46831060c817d475b54a8
akshayk122 <myselfakshay22@gmail.com>
2025-08-30 13:07:03 -0500

    health tools update

--- personal_assistant/mcp_tools/health_diet_tools.py
+++ personal_assistant/mcp_tools/health_diet_tools.py
@@ -305,7 +305,7 @@ def get_food_log() -> str:
                     meals[meal_type] = []
                 meals[meal_type].append(food)
             
-            result = "Today's Food Log\n\n"
+            result = "Today's Log\n\n"
             total_calories = 0
             
             for meal_type, foods in meals.items():
